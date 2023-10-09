f8989e9ae420ab34a65235de233f64dc41998b2c
kamruzzaman leeon <48612426+kamruzzaman-leeon@users.noreply.github.com>
2023-10-09 09:36:41 +0600

    Update DonatedPage.jsx
    propagation added

--- src/Components/DonatedPage.jsx
+++ src/Components/DonatedPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { getStoredMyDonation } from './LocalStorageMyDonation';
 import MyDonated from './MyDonated';
@@ -6,32 +6,44 @@ import MyDonated from './MyDonated';
 const DonatedPage = () => {
     const donates = useLoaderData();
     const [myDonate, setMyDonate] = useState([]);
+    const [showAll, setShowAll] = useState(false);
 
     useEffect(() => {
         const storedMyDonationId = getStoredMyDonation();
 
         if (donates.length > 0 && storedMyDonationId.length > 0) {
             const donated = [];
-            // console.log(storedMyDonationId)
             for (const data of storedMyDonationId) {
-                const donate = donates.find(donate => donate.id == data.id);
+                const donate = donates.find(donate => donate.id === data.id);
                 if (donate) {
                     donated.push(donate);
                 }
             }
             setMyDonate(donated);
         } else {
-            
             setMyDonate([]);
         }
     }, [donates]);
-    // console.log(myDonate)
+
+    const toggleShowAll = () => {
+        setShowAll(!showAll);
+    };
+
     return (
+        <>
         <div className='content-box grid grid-col-1 md:grid-cols-2 gap-10'>
-            {myDonate.map(donate => (
+            {myDonate.slice(0, showAll ? myDonate.length : 4).map(donate => (
                 <MyDonated key={donate.id} donate={donate} />
             ))}
+           
         </div>
+        {myDonate.length >= 4 && (
+                <div className='text-center'><button className='btn btn-info' onClick={toggleShowAll}>
+                {showAll ? 'Show Less' : 'Show All'}
+            </button></div>
+            )}
+        </>
+        
     );
 };
 
